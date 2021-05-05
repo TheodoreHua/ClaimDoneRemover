8e8ac75340dc8600bb96d4ce87befd4a59013124
Theodore Hua <theodorehuadev@gmail.com>
2021-05-05 13:06:08 -0400

    Bump version

--- helpers/global_vars.py
+++ helpers/global_vars.py
@@ -13,7 +13,7 @@ from tkinter.messagebox import showerror
 home = expanduser("~")
 PLATFORM_LOCATIONS = {"linux": ".config",
                       "darwin": ".config"}
-VERSION = "4.14.73"
+VERSION = "4.14.77"
 if sysplatform.startswith("win"):
     OS = "Windows"
     DATA_PATH = environ["APPDATA"] + "\\ClaimDoneRemover"
